d66d5ddab7e924077977e25d87655ed4231b5d86
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-22 18:30:08 +0100

    Add

--- count.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for i in os.scandir():
-     i = str(i)
-     i = i.replace('<', '')
-     i = i.replace('>', '')
-     i = i.replace("DirEntry ", '')
-     i = i.replace("'", "")
-     print(i)
